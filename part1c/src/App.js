5234be52384d2f011ec7e0d821afd4f1c2d2b79e
Gio <giofra.dev@gmail.com>
2023-03-04 23:20:19 +0200

    part1c Destructuring

--- part1c/src/App.js
+++ part1c/src/App.js
@@ -1,9 +1,30 @@
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  // OR
+  // const { name, age } = props;
+
+  // OR;
+  // const name = props.name;
+  // const age = props.age;
+
+  const bornYear = () => new Date().getFullYear() - age;
+
+  // OR
+  // const bornYear = () => {
+  //   return new Date().getFullYear() - age;
+  // };
+
+  // const bornYear = () => {
+  //   const yearNow = new Date().getFullYear();
+
+  //   return yearNow - props.age;
+  // };
+
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 };
