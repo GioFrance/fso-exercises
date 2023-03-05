04dd4ced2213f86184ffcca15d5565ac27995884
Gio <giofra.dev@gmail.com>
2023-03-05 22:45:16 +0200

    part1d Handling Arras

--- part1d/src/App.js
+++ part1d/src/App.js
@@ -1,15 +1,48 @@
 import { useState } from "react";
 
 const App = () => {
+  // const [clicks, setClicks] = useState({
+  //   left: 0,
+  //   right: 0,
+  // });
+  // OR
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  // const handleLeftClick = () => {
+  //   const newClicks = {
+  //     ...clicks,
+  //     left: clicks.left + 1,
+  //   };
+  //   setClicks(newClicks);
+  // };
+  // OR
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
+  // OR
+  // const handleRightClick = () => {
+  //   const newClicks = {
+  //     ...clicks,
+  //     right: clicks.right + 1,
+  //   };
+  //   setClicks(newClicks);
+  // };
 
   return (
     <div>
       {left}
-      <button onClick={() => setLeft(left + 1)}>left</button>
-      <button onClick={() => setRight(right + 1)}>right</button>
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
       {right}
+      <p>{allClicks.join(" ")}</p>
     </div>
   );
 };
