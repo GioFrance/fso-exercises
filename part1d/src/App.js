29a26f0d35bb3b087e137ea72d09aeb1da2556de
Gio <giofra.dev@gmail.com>
2023-03-06 14:22:40 +0200

    Function that returns a function

--- part1d/src/App.js
+++ part1d/src/App.js
@@ -97,28 +97,73 @@
 
 //  ------------------------------------
 
+// import { useState } from "react";
+
+// const App = () => {
+//   const [value, setValue] = useState(10);
+
+//   const zero = () => {
+//     console.log("clicked the button");
+//     setValue(0);
+//   };
+
+//   return (
+//     <div>
+//       {value}
+//       <button onClick={zero}>reset to zero</button>
+//       OR
+//       <button onClick={() => console.log("clicked the button")}>
+//         reset to zero
+//       </button>
+//       OR
+//       <button onClick={() => setValue(0)}>reset to zero</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+//  ------------------------------------
+
 import { useState } from "react";
 
 const App = () => {
   const [value, setValue] = useState(10);
 
-  const zero = () => {
-    console.log("clicked the button");
-    setValue(0);
+  const setToValue = (newValue) => () => {
+    console.log("value now", newValue); // print the new value to console
+    setValue(newValue);
   };
 
   return (
     <div>
       {value}
-      <button onClick={zero}>reset to zero</button>
-      OR
-      <button onClick={() => console.log("clicked the button")}>
-        reset to zero
-      </button>
-      OR
-      <button onClick={() => setValue(0)}>reset to zero</button>
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   );
+
+  // OR
+  // const setToValue = (newValue) => {
+  //   console.log('value now', newValue)
+  //   setValue(newValue)
+  // }
+
+  // return (
+  //   <div>
+  //     {value}
+  //     <button onClick={() => setToValue(1000)}>
+  //       thousand
+  //     </button>
+  //     <button onClick={() => setToValue(0)}>
+  //       reset
+  //     </button>
+  //     <button onClick={() => setToValue(value + 1)}>
+  //       increment
+  //     </button>
+  //   </div>
+  // )
 };
 
 export default App;
