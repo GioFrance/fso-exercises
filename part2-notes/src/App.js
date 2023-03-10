dd930e799bc4fc697556cf5c6ae7de1dc22fd9e3
Gio <giofra.dev@gmail.com>
2023-03-10 23:12:07 +0200

    part2b Filtering Displayed Elements Conditional

--- part2-notes/src/App.js
+++ part2-notes/src/App.js
@@ -22,13 +22,16 @@ const App = (props) => {
     setNewNote(event.target.value);
   };
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important === true);
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
         {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
