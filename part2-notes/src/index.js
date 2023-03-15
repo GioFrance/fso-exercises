76f597e73fe21d3d38080e6bbfe53a7e31dbfffd
Gio <giofra.dev@gmail.com>
2023-03-15 14:34:14 +0200

    part2c Axios and promises

--- part2-notes/src/index.js
+++ part2-notes/src/index.js
@@ -4,38 +4,9 @@ import axios from "axios";
 
 import App from "./App";
 
-// const promise = axios.get("http://localhost:3001/notes");
-
-// promise.then((response) => {
-//   console.log(response);
-// });
-
 axios.get("http://localhost:3001/notes").then((response) => {
   const notes = response.data;
-  console.log(notes);
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <App noted={notes} />
+  );
 });
-
-const promise2 = axios.get("http://localhost:3001/foobar");
-console.log(promise2);
-
-const notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App noted={notes} />
-);
