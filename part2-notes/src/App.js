91392024666399134020b60307b308acad116507
Gio <giofra.dev@gmail.com>
2023-03-16 14:22:17 +0200

    part2d Changing the Importance of Notes

--- part2-notes/src/App.js
+++ part2-notes/src/App.js
@@ -29,6 +29,16 @@ const App = () => {
     });
   };
 
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((n) => (n.id !== id ? n : response.data)));
+    });
+  };
+
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value);
@@ -46,7 +56,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
